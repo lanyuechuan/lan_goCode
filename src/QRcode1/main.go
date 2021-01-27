24aeadf669637853fc277d210be67cf864082d56
lanyuechuan <626459361@qq.com>
2021-01-27 11:08:01 +0800

    feat: golangurl转二维码八涩

--- src/QRcode1/main.go
+++ src/QRcode1/main.go
@@ -1,28 +1,21 @@
 package main
 
 import (
-	_ "fmt"
 	"net/http"
-
+	"encoding/base64"
 	"github.com/gin-gonic/gin"
 	qrcode "github.com/skip2/go-qrcode"
-	"encoding/base64"
-	// 	"io/ioutil"
+	"unsafe"
 	"os"
 )
 
 // 定义接收数据的结构体
 type QrCode struct {
-    Url	string 
+    Url	string `form:"url" json:"url" uri:"url" xml:"url" binding:"required"`
 }
 
 
-type msg struct {
-	Name []byte
-}
-
-
-func ImagesToBase64() []byte {
+func ImagesToBase64() string {
 	//读原图片
 	ff, _ := os.Open("./policy_qrcode.png")
 	defer ff.Close()
@@ -30,7 +23,10 @@ func ImagesToBase64() []byte {
 	n, _ := ff.Read(sourcebuffer)
 	//base64压缩
 	sourcestring := base64.StdEncoding.EncodeToString(sourcebuffer[:n])
-	return []byte(sourcestring)
+	baseImage := []byte(sourcestring)
+	// *(*string)(unsafe.Pointer(&baseImage)) --->切片转字符串
+	return "data:image/png;base64," + *(*string)(unsafe.Pointer(&baseImage))
+
 }
 
 
@@ -39,14 +35,16 @@ func main() {
 	r := gin.Default()
 	// 2.绑定路由规则，执行的函数
 	r.POST("/policy/url-qrcode", func(c *gin.Context) {
-		// var form QrCode
-		url := c.PostForm("Url")
+		var form QrCode
+		url := c.PostForm("url")
+		if err := c.Bind(&form); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error":err.Error(), "status":"400"})
+			return
+		}
 		qrcode.WriteFile(url, qrcode.Medium, 256, "./policy_qrcode.png")
 
-		data := msg{
-			ImagesToBase64(),
-		}
-		c.JSON(http.StatusOK, data)
+		url_code := ImagesToBase64()
+		c.JSON(http.StatusOK, gin.H{"url_code":url_code, "status":"200"})
 	})
 	// 3.监听端口，默认在8080。Run("里面不指定端口号默认为8080")
 	r.Run(":8005")
