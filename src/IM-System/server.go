84eba99e0c9b65446d1ad484d597bfbeb8dd160c
lanyuechuan <lanyuechun592@163.com>
2021-03-01 23:13:07 +0800

    feat: 版本一，创建一个服务器端监听某个ip和端口，并用协程来处理 一个连接成功后的回调

---
+++ src/IM-System/server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+
+type Server struct {
+	Ip string
+	Port int
+}
+
+//创建一个server的接口
+func NewServer(ip string, port int) *Server{
+	server := &Server{
+		Ip : ip,
+		Port : port,
+	}
+	return server
+}
+
+func (this *Server) Handler(conn net.Conn) {
+	//当前连接的业务
+	fmt.Println("连接建立成功")
+}
+
+//启动服务器的接口
+func (this *Server) Start() {
+	//socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d",this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("监听状态启动失败",err)
+		return
+	}
+
+	//close listen socket
+	defer listener.Close()
+
+	for {
+		//accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("某个客户端进来，但是连接失败", err)
+			continue //继续等客户端连接
+		}
+
+		//如果成功，则执行当前连接成功后的业务。do handler 回调
+		go this.Handler(conn)
+	}
+	
+}
