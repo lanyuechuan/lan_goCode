c31e8d6d1d0c4559f02d5317177d0d1c8a9b7727
lanyuechuan <lanyuechun592@163.com>
2021-03-03 23:06:03 +0800

    feat: 在线用户消息系统

--- src/IM-System/server.go
+++ src/IM-System/server.go
@@ -9,20 +9,63 @@ import (
 type Server struct {
 	Ip string
 	Port int
-}
+	
+	//在线用户的列表
+	OnlineMap map[string]*User
+	mapLock sync.RWMutex //读写锁
+
+	//消息广播的channel
+	Message chan string
 
 //创建一个server的接口
 func NewServer(ip string, port int) *Server{
 	server := &Server{
 		Ip : ip,
 		Port : port,
+		OnlineMap: make(map[string]*User),
+		Message: make(chan string)
 	}
 	return server
 }
 
+//监听Message广播消息channel的goroutine，一旦有消息就发送给全部的在线User
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+
+		//将msg发送给全部的在线User
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+
+//广播消息的方法
+func(this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <-sendMsg
+}
+
 func (this *Server) Handler(conn net.Conn) {
 	//当前连接的业务
-	fmt.Println("连接建立成功")
+	// fmt.Println("连接建立成功")
+
+	user := NewUser(conn)
+
+	//用户上线，将用户加入到onlineMap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+	
+	//广播当前用户上线消息
+	this.BroadCast(user,"已上线")
+
+	//当前handler阻塞
+	select {}
+
 }
 
 //启动服务器的接口
@@ -36,6 +79,9 @@ func (this *Server) Start() {
 
 	//close listen socket
 	defer listener.Close()
+
+	//启动监听Message的goroutine
+	go this.ListenMessage()
 
 	for {
 		//accept
---
+++ src/IM-System/user.go
@@ -0,0 +1,33 @@
+package main
+
+import "net"
+
+type User struct {
+	Name string
+	Addr string
+	C    chan string
+	conn net.Conn
+}
+
+//创建用户的接口
+func NewUser(conn net.Conn) *User {
+	userAddr := conn.RemoteAddr().String() //拿到客户端的地址
+	user := &User {
+		Name: userAddr,
+		Addr: userAddr,
+		conn: make(chan string),
+		conn: conn,
+	}
+
+	//启动监听当前user channel消息的rotoutine
+	go user.ListenMessage()
+	return user
+}
+
+//监听当前User channel的方法，一旦有消息，就直接发送给客户端
+func (this *User) ListenMessage() {
+	for {
+		msg := <-this.C
+		this.conn.Write([]byte(msg + "\n"))
+	}
+}
